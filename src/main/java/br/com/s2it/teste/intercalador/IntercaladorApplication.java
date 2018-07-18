bb01d3703aa0a8b52a63368db2256017a0e0ce2a
Alex Garcia <agarcia46@dxc.com>
2018-07-18 15:54:43 -0300

    Estrutura inicial criada através do Spring Initializer.

---
+++ src/main/java/br/com/s2it/teste/intercalador/IntercaladorApplication.java
@@ -0,0 +1,12 @@
+package br.com.s2it.teste.intercalador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercaladorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercaladorApplication.class, args);
+	}
+}
